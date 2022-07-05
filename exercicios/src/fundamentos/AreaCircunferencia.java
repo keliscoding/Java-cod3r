fdee3221458ad2294f26ecaa49eb0b456641cebd
Kelly Castelo <kawokkun@gmail.com>
2022-07-05 06:22:18 -0300

    desafio variaveis

--- exercicios/src/fundamentos/AreaCircunferencia.java
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -1,5 +1,18 @@
 package fundamentos;
 
 public class AreaCircunferencia {
+	public static void main(String[] args) {
+		int raio = 3;
+		
+		//final == const
+		final double PI = 3.4;
+		
+		double area = PI * raio * raio;
 
+		System.out.println(area);
+		
+		raio = 10;
+		area = PI * raio * raio;
+		System.out.println("Area = " + area + "m2");
+	}
 }
