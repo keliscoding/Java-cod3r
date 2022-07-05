7b05dadb9d733144c6de9eba3368b1029f0085be
Kelly Castelo <kawokkun@gmail.com>
2022-07-05 09:39:59 -0300

    DesafiosAritmeticos

---
+++ exercicios/src/fundamentos/DesafiosAritmeticos.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class DesafiosAritmeticos {
+	public static void main(String[] args) {
+		
+		final int firstOp = (int) Math.pow((6 * (3 + 2)), 2)/(3*2);
+		final int secondOp = (int) Math.pow((1-5) * (2-7)/2, 2);
+		
+		final int thirdOp = firstOp - secondOp;
+		final int fourthOp = (int) Math.pow(thirdOp, 3);
+		final int fifthOp = (int) Math.pow(10, 3);
+		
+		final int answer = fourthOp/fifthOp;
+		System.out.println(answer);
+	}
+}
