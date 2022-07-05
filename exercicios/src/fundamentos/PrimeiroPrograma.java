f419d5e12bb93f83e9032d1083c5b27185e74b1b
Kelly Castelo <kawokkun@gmail.com>
2022-07-05 06:14:27 -0300

    Comentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,27 @@
 package fundamentos;
 
+/**
+ * Javadoc é gerado usando "/** + enter"
+ * @author zam0k
+ * 	@since JDK 1.0
+ */
 public class PrimeiroPrograma {
-	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println("Primeiro programa!!!");
+
+		// Comentário de uma linha
+
+		/*
+		 * Comentário De Multiplas Linhas
+		 */
+
+//		 System.out.println("Código comentado");
+		
+		
+
+		System.out.println("Primeiro programa!!!"); //Aqui tb funciona
 	}
 }
