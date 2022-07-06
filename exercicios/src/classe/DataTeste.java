43cc362b6134069581663df6a944d6f1d8de9858
Kelly Castelo <kawokkun@gmail.com>
2022-07-06 08:20:41 -0300

    Desafio Classe Data

---
+++ exercicios/src/classe/DataTeste.java
@@ -0,0 +1,18 @@
+package classe;
+
+public class DataTeste {
+	public static void main(String[] args) {
+		
+		Data d1 = new Data();
+		
+		d1.dia = 1;
+		d1.mes = 8;
+		d1.ano = 1997;
+		
+		Data d2 = new Data();
+		
+		d2.dia = 15;
+		d2.mes = 10;
+		d2.ano = 1997;
+	}
+}
