16515b1c2a87accf51e92ef7ece231dc80d02270
Kelly Castelo <kawokkun@gmail.com>
2022-07-10 15:21:55 -0300

    Herança #01

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -2,15 +2,27 @@ package oo.heranca;
 
 public class Jogo {
 	public static void main(String[] args) {
-		Jogador jogador1 = new Jogador();
+		Jogador jogador1 = new Monstro();
 		jogador1.x = 10;
-		jogador1.y = 20;
+		jogador1.y = 10;
 		
-		jogador1.andar(Direcao.NORTE);
-		jogador1.andar(Direcao.LESTE);
-		jogador1.andar(Direcao.NORTE);
-		jogador1.andar(Direcao.LESTE);
+		Jogador jogador2 = new Heroi();
+		jogador2.x = 10;
+		jogador2.y = 11;
 		
-		System.out.println(jogador1.x + " " + jogador1.y);
+//		jogador1.andar(Direcao.NORTE);
+//		jogador1.andar(Direcao.LESTE);
+//		jogador1.andar(Direcao.NORTE);
+//		jogador1.andar(Direcao.LESTE);
+//		System.out.println(jogador1.x + " " + jogador1.y);
+		
+		System.out.println(jogador1.vida);
+		System.out.println(jogador2.vida);
+		
+		jogador1.atacar(jogador2);
+		
+		System.out.println(jogador1.vida);
+		System.out.println(jogador2.vida);
+			
 	}
 }
